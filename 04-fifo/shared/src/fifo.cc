614f46003c7d49235259b146778ed4339a8b610f
timmer <timmer@castar.com>
2016-09-13 12:13:18 -0700

    udpate comments. add lock free queue example. hee hee.

--- 04-fifo/shared/src/fifo.cc
+++ 04-fifo/shared/src/fifo.cc
@@ -53,6 +53,12 @@ actually switched out.
 get will find H == T and return.
 it will not matter that *H contains a valid pointer.
 
+assumption:
+the compiler won't re-order volatile memory accesses.
+head, tail, and the pointers in the array are volatile.
+so the compiler should emit the instructions for put
+in the desired order.
+
 assumes the library lock and unlock functions contain
 fences that flush the memory write operations.
 */
@@ -75,9 +81,9 @@ namespace {
             delete[] data_;
         }
 
-        int data_size_ = 0;
-        int head_ = 0;
-        int tail_ = 0;
+        int size_ = 0;
+        int volatile head_ = 0;
+        int volatile tail_ = 0;
         char * volatile *data_ = nullptr;
         std::mutex head_mutex_;
         std::mutex tail_mutex_;
@@ -103,7 +109,7 @@ Fifo *Fifo::create(
 ) throw() {
     auto impl = new(std::nothrow) FifoImpl;
     auto size = maxCount + 1;
-    impl->data_size_ = size;
+    impl->size_ = size;
     impl->head_ = 0;
     impl->head_ = 0;
     impl->data_ = new(std::nothrow) char*[size];
@@ -115,7 +121,7 @@ returns maxCount passed to the constructor.
 */
 int Fifo::getMaxCount() throw() {
     auto impl = (FifoImpl *) this;
-    auto count = impl->data_size_ - 1;
+    auto count = impl->size_ - 1;
     return count;
 }
 
@@ -133,7 +139,7 @@ bool Fifo::put(
     bool result = false;
     {
         std::unique_lock<std::mutex> lock(impl->tail_mutex_);
-        auto new_tail = (impl->tail_ + 1) % impl->data_size_;
+        auto new_tail = (impl->tail_ + 1) % impl->size_;
         if (new_tail != impl->head_) {
             impl->data_[impl->tail_] = ptr;
             impl->tail_ = new_tail;
@@ -159,7 +165,7 @@ char *Fifo::get() throw() {
             ptr = impl->data_[impl->head_];
             if (ptr) {
                 impl->data_[impl->head_] = nullptr;
-                impl->head_ = (impl->head_ + 1) % impl->data_size_;
+                impl->head_ = (impl->head_ + 1) % impl->size_;
             }
         }
     }
@@ -186,7 +192,7 @@ char *Fifo::getWait() throw() {
             ptr = impl->data_[impl->head_];
         } while (ptr == nullptr);
         impl->data_[impl->head_] = nullptr;
-        impl->head_ = (impl->head_ + 1) % impl->data_size_;
+        impl->head_ = (impl->head_ + 1) % impl->size_;
     }
     return ptr;
 }
